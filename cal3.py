667798c55f514ad3e3e12f0d195f063745490b4e
HIKARUsdf1 <marcosmark9100@gmail.com>
2026-08-05 12:26:50 -0500

    PRACTICA AUTOsELF

---
+++ cal3.py
@@ -0,0 +1,8 @@
+
+
+
+print(" Bienvenido a la Calculadora de PYTHON\n\n")
+num1 = int(input("Escribe un numero:   "))
+num2 = int(input("Ingresa segundo numero:   "))
+resultado = num1 + num2
+print("La suma es:   ", resultado)
