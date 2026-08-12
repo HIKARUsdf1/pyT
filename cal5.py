2dce273c8bd710533fb4e6f1631b0dbbd8218109
HIKARUsdf1 <marcosmark9100@gmail.com>
2026-08-11 20:16:08 -0500

    SIN GIT CLONE

---
+++ cal5.py
@@ -0,0 +1,13 @@
+
+
+print(" Bienvenido a la Calculadora de PYTHON\n\n")
+num1 = int(input("Escribe un numero:   "))
+num2 = int(input("Ingresa segundo numero:   "))
+resultado = num1 + num2
+print("La suma es:   ", resultado)
+
+print("\n\n SIN UTILIZAR GUT CLONE VEAMOS\n\n")
+
+
+
+
