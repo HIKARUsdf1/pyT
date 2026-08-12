e6e0e1bb0a7259d71f5d22de19d039997edd3e51
HIKARUsdf1 <marcosmark9100@gmail.com>
2026-08-11 20:06:40 -0500

    11 AGOSTO 2026

--- cal4.py
+++ cal4.py
@@ -1,10 +1,11 @@
 
 
+
+
 print(" Bienvenido a la Calculadora de PYTHON\n\n")
-
-print(" NUEVA PRACTICA  11/08/2026\n\n")
-
 num1 = int(input("Escribe un numero:   "))
 num2 = int(input("Ingresa segundo numero:   "))
 resultado = num1 + num2
 print("La suma es:   ", resultado)
+
+print(" INTENTO 11 AGOSTO 2026\n\n")
